784c136b7818eadf83b796860a2c0de2464e0392
Thiago-Portella <thiagoportellaferreira@gmail.com>
2023-06-04 08:42:54 -0300

    1006 - Average 2

---
+++ src/main/kotlin/beecrowd/1006/1006.kt
@@ -0,0 +1,7 @@
+package beecrowd.`1006`
+fun main() {
+    val A = readLine()!!.toDouble()
+    val B = readLine()!!.toDouble()
+    val C = readLine()!!.toDouble()
+    println("MEDIA = %.1f".format(A * 0.2 + B * 0.3 + C * 0.5))
+}
